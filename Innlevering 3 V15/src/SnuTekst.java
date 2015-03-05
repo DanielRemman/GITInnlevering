001039e1d057d5043ff794fa21b3fc385fd65828
DanielRemman <daniel@vremman.no>
2015-03-05 12:07:26 +0100

    SnuTekstv0.1

---
+++ Innlevering 3 V15/src/SnuTekst.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SnuTekst {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Skriv inn en streng: ");
+		String temp = input.nextLine();
+		System.out.println(temp);
+		baklengs(temp);
+	}
+
+	public static void baklengs(String tekst) {
+		int tekstLengde = tekst.length() - 1;
+		if (tekstLengde >= 0) {
+			System.out.print(tekst.charAt(tekstLengde));
+			baklengs(tekst.substring(0, tekstLengde));
+		} else
+			System.out.println();
+	}
+}
