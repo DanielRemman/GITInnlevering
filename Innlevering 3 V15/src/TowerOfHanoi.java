27aa385ae88c2d7668d69c16af35054a144cc56d
DanielRemman <daniel@vremman.no>
2015-03-05 13:00:59 +0100

    TowerOfHanoi_v0.3

--- Innlevering 3 V15/src/TowerOfHanoi.java
+++ Innlevering 3 V15/src/TowerOfHanoi.java
@@ -4,6 +4,7 @@ public class TowerOfHanoi {
 
 	private static Scanner input = new Scanner(System.in);
 	private static long antallTrekk;
+	private static long antallKall = 1;
 
 	public static void main(String[] args) {
 		System.out.print("Skriv inn antall skiver: ");
@@ -11,9 +12,11 @@ public class TowerOfHanoi {
 		System.out.println("\nTrekkene er: ");
 		flyttBrikke(n, 'A', 'B', 'C');
 		System.out.println("Antall trekk: " + antallTrekk);
+		System.out.println("Antall kall: " + antallKall);
 	}
 
 	private static void flyttBrikke(int n, char fraTårn, char tilTårn, char temp) {
+		antallKall++;
 		if (n == 1){
 			System.out.printf("Flytt brikke %2d fra %c til %c\n", n, fraTårn,
 					tilTårn);
